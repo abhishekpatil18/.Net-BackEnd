4f0f4b4a0723b450158698d974ea30920b8baa71
Abhishek Patil <Abhishek.Patil@emtecinc.com>
2022-11-11 15:37:03 +0530

    Created WEB_API project to do arithmetic operations.

---
+++ API_Calculator/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace API_Calculator
+{
+    public  class Class1
+    {
+        public static string Add(int n1,int n2)
+        {
+            return $"Addition of {n1} & {n2} is : {n1 + n2} ";
+        }
+
+        public static string Sub(int n1, int n2)
+        {
+            return $"Substraction of {n1} & {n2} is : {n1 - n2}";
+        }
+    }
+}
---
+++ WEB_API_App1/Controllers/CalcController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_API_App1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalcController : ControllerBase
+    {
+        // GET: api/Calc
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] {
+            API_Calculator.Class1.Add(6, 8),
+            API_Calculator.Class1.Sub(90, 27),
+         };
+        }
+
+        //// GET api/Calc/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return $"value {id}";
+        //}
+
+        //// POST api/Calc
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/Calc/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/Calc/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
