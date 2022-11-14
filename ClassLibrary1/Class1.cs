b51e7503dc58343fd3bd40931db6b2c27dabe7e7
Abhishek Patil <Abhishek.Patil@emtecinc.com>
2022-11-14 13:52:49 +0530

    Created WEB API Project using .Net Framework.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public void Add(int n1,int n2)
+        {
+            Console.WriteLine("Addition is : "+(n1+n2));
+        }
+    }
+}
---
+++ WebApplicationFrameWork/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplicationFrameWork.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            //ClassLibrary1.Class1.Add(90, 89);
+            return new string[] { "Value1","Value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
