6b950ee8777ef5df62d3e09bf71f5d04925f22b7
Abhishek Patil <Abhishek.Patil@emtecinc.com>
2022-11-22 10:36:15 +0530

    Created ADO.Net and Dapper assignment projects

---
+++ Ado.NetApp/Repositorys/EmployeeRepository.cs
@@ -0,0 +1,102 @@
+﻿using Ado.NetApp.Models;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace Ado.NetApp.Repositorys
+{
+    public class EmployeeRepository
+    {
+        private readonly string _connectionString;
+        private IConfiguration Configuration;
+
+        public EmployeeRepository(IConfiguration config)
+        {
+            Configuration = config;
+            _connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+        }
+
+       public IEnumerable<EmployeeData> Get()
+        {
+            var employees = new List<EmployeeData>();
+            DataTable employeeTable = new DataTable();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                
+                string query = "SELECT * FROM Employee2";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(employeeTable);
+            }
+
+            for(int i = 0; i < employeeTable.Rows.Count; i++)
+            {
+                EmployeeData emp = new EmployeeData()
+                {
+                    Id = (int)employeeTable.Rows[i]["Id"],
+                    Name = (string)employeeTable.Rows[i]["Name"],
+                    Department = (string)employeeTable.Rows[i]["Department"],
+                    Salary = (int)employeeTable.Rows[i]["Salary"],
+
+                };
+                employees.Add(emp);
+            }
+            return employees;
+        }
+
+        public bool Post (EmployeeData emp)
+        {
+
+             using (var connection = new SqlConnection(_connectionString))
+            {
+                string query = $"insert into Employee2(Name,Department,Salary) values ('{emp.Name}','{emp.Department}','{emp.Salary}')";
+
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+
+            return true;
+        }
+
+        public bool Update(EmployeeData emp,int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                string query = $"UPDATE Employee2 set Name='{emp.Name}',Department='{emp.Department}',Salary='{emp.Salary}' Where Id='{id}'";
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.CommandType= CommandType.Text;
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                string query = $"DELETE FROM Employee2 where Id='{id}'";
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ DapperAssignment/Controllers/EmployeeController.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using DapperAssignment.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace DapperAssignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+           private IConfiguration _configuration;
+
+        public EmployeeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> Get()
+        {
+            using var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            var emp = await connection.QueryAsync<Employee>("SELECT * FROM Employee2");
+            return Ok(emp);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateEmployee(Employee emp)
+        {
+            using var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            var newEmp = await connection.ExecuteAsync("INSERT INTO Employee2 (Name,Department,Salary)values (@Name,@Department,@Salary)", emp);
+            return Ok(newEmp);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> updateEmployee(Employee emp)
+        {
+            using var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            var updatedEmp = await connection.ExecuteAsync("Update Employee2 set Name=@Name,Department=@Department,Salary=@Salary where Id=@Id", emp);
+            return Ok(updatedEmp);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> deleteEmployee(int id)
+        {
+            using var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            var deletedEmp = await connection.ExecuteAsync("Delete from Employee2 where Id=@Id", new { Id=id });
+            return Ok(deletedEmp);
+        }
+
+    }
+}
