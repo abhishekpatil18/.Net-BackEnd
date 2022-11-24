1f3b316d3387a9581c82762f0051a7e950a373bc
Abhishek Patil <Abhishek.Patil@emtecinc.com>
2022-11-24 18:53:42 +0530

    Created Console calculator application

---
+++ ConsoleCalc/CalcModel/Calc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalc.CalcModel
+{
+    internal class Calc
+    {
+        public string Calculate()
+        {
+            Console.WriteLine("Enter First Number");
+            string n1 = Console.ReadLine();
+
+            Console.WriteLine("Enter Second Number");
+            string n2= Console.ReadLine();
+
+            Console.WriteLine("Enter Operator");
+            string op = Console.ReadLine();
+
+            
+            switch (op)
+            {
+                case "+":
+                   return $"{Convert.ToInt32(n1) + Convert.ToInt32(n2)}";
+                case "-":
+                    return $"{Convert.ToInt32(n1) - Convert.ToInt32(n2)}";
+                case "*":
+                    return $"{Convert.ToInt32(n1) * Convert.ToInt32(n2)}";
+                case "/":
+                    return $"{Convert.ToInt32(n1) / Convert.ToInt32(n2)}";
+                default:
+                   return "Enter Correct details";
+            }
+
+        }
+    }
+}
