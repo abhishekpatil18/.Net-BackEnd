c2553c8503ebc7b3615976cf0ed1caaeb06e9db4
Abhishek Patil <Abhishek.Patil@emtecinc.com>
2022-11-24 18:14:47 +0530

    Created Calculator project using Asp.Net Framework.

--- WebApplicationFrameWork/Controllers/ValuesController.cs
+++ WebApplicationFrameWork/Controllers/ValuesController.cs
@@ -7,34 +7,19 @@ using System.Web.Http;
 
 namespace WebApplicationFrameWork.Controllers
 {
-    public class ValuesController : ApiController
+    public class CalculatorController : ApiController
     {
-        // GET api/values
+        
         public IEnumerable<string> Get()
         {
-            //ClassLibrary1.Class1.Add(90, 89);
-            return new string[] { "Value1","Value2" };
+            return new string[] {
+                CalcFunctions.CalcOperation.Add(10,20),
+                CalcFunctions.CalcOperation.Sub(89,67),
+                CalcFunctions.CalcOperation.Mult(10,90),
+                CalcFunctions.CalcOperation.Div(78,9),
+            };
         }
 
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
 
-        // POST api/values
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
     }
 }
